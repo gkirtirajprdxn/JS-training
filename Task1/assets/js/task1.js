fdb6f548d25e3f4b9d060f1d0104735dd7126671
Kirtiraj <kirtirajg1997@gmail.com>
2020-08-20 12:45:36 +0530

    JS Task1 updated

--- Task1/assets/js/task1.js
+++ Task1/assets/js/task1.js
@@ -1,14 +1,11 @@
-var arr = [12,45,22,66,86,32,13,65,99,43];
+var arr = [12,45,22,66,86,43,13,65,99,43];
 var n = prompt("Enter the No. :- ");
 
-breakall:
 for (let i = 0; i <= arr.length-1; i++){
 	for (let j = i+1; j <= arr.length-1; j++){
 		if(arr[i] + arr[j] == n){
 			document.write("The nos. are ",arr[i], " & ",arr[j]);
-		}else{
-			document.write("No such numbers found!");
-			break breakall;
+			document.write('<br>');
 		}
 	}
 }

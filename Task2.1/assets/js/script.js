f6cb4d7284483357ca966747d9bced7327470143
Kirtiraj <kirtirajg1997@gmail.com>
2020-08-25 17:20:29 +0530

    function insertRecord added

--- Task2.1/assets/js/script.js
+++ Task2.1/assets/js/script.js
@@ -14,3 +14,21 @@ function readFormData(){
 	formData['address'] = document.getElementById('address').value;
 	return formData;
 }
+
+function insertRecord(data){
+	var table = document.getElementById('elist').getElementsByTagName('tbody')[0];
+	var row = table.insertRow(table.length);
+	cell1 = row.insertCell(0);
+	cell1.innerHTML = data.firstname;
+	cell2 = row.insertCell(1);
+	cell2.innerHTML = data.lastname;
+	cell3 = row.insertCell(2);
+	cell3.innerHTML = data.gender;
+	cell4 = row.insertCell(3);
+	cell4.innerHTML = data.address;
+	cell4 = row.insertCell(4);
+	cell4.innerHTML = '<a onClick="onEdit(this)">Edit</a>';
+	cell5 = row.insertCell(5);
+	cell5.innerHTML = '<a onClick="onDelete(this)">Delete</a>';
+	alert("Thank you!");
+}

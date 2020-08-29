3c2924514aa74cbcf5af3029bea6160bfb2ee656
Kirtiraj <kirtirajg1997@gmail.com>
2020-08-29 17:21:34 +0530

    Created load more button with its function

--- Task3.1/assets/js/script.js
+++ Task3.1/assets/js/script.js
@@ -16,3 +16,31 @@ xhr.onreadystatechange = function() {
 	}
 }
 xhr.send();
+
+var btn = document.getElementById('load-more');
+btn.addEventListener('click',onloadmore);
+
+function onloadmore(){
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var posts = JSON.parse(xhr.responseText);
+            for (var i=index; i<len; i++){
+                var string = "title "+(i+1)+": "+posts[i].title +".\n"+"description "+(i+1)+": "+posts[i].body;
+                if (i>=6){
+                    var list = document.createElement('li');
+                    var text = document.createTextNode(string);
+                    list.appendChild(text);
+                    document.getElementById('ulist').appendChild(list);
+                    if (i == posts.length-1){
+                        document.getElementById('load-more').style.display = 'none';
+                        break;
+                    }
+                }
+            }
+            index = index+6;
+            len = len+6;
+        }
+    }
+    xhr.send();
+}
